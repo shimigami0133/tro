2b0c9292577a636a4166f2229173c9ea5f4a7659
abhiram reddy <shimigami0133@gmail.com>
2021-06-10 18:33:07 +0530

    add

---
+++ o.java
@@ -0,0 +1,13 @@
+class test{
+
+    public int o(int i,int j)
+    {
+        return i+j;
+    }
+    public static void main(String args [])
+    {
+        test t1=new test();
+        System.out.println(t1.o(1,2));
+    }
+   
+}
