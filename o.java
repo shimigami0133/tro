cbe6d05120719fe8fc18bb793f16ac6884873c2d
abhiram reddy <shimigami0133@gmail.com>
2021-06-10 18:55:40 +0530

    dlslkfj

--- o.java
+++ o.java
@@ -1,13 +1,12 @@
-class test{
+class test {
 
-    public int o(int i,int j)
-    {
-        return i/j;
+    public int o(int i, int j) {
+        return i * j;
     }
-    public static void main(String args [])
-    {
-        test t1=new test();
-        System.out.println(t1.o(1,2));
+
+    public static void main(String args[]) {
+        test t1 = new test();
+        System.out.println(t1.o(1, 2));
     }
-   
+
 }
